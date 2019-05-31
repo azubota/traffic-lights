d2b8e94d5bb4c5828b3c1e10ff14d515b8d92ea3
Azul Botaya <admbotaya@gmail.com>
2019-05-31 03:17:12 +0000

    firstcommit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+//import your own components
+import Trafficlight from "./component/Trafficlight.jsx";
+
+//render your react application
+ReactDOM.render(<Trafficlight />, document.querySelector("#app"));
